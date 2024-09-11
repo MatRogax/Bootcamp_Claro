ae82f1f8d980fad7f6a792b17ff7cd9c19463e28
Matheus Rogato <matheusrogato@gmail.com>
2024-09-11 20:23:46 -0300

    feat: projeto de conta bancaria para o bootcamp da claro

---
+++ App/src/account/Account.java
@@ -0,0 +1,25 @@
+package account;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    public int number;
+    public String agency;
+    public String name;
+    public BigDecimal balance;
+
+    public Account(int number,String agency,String name,BigDecimal balance){
+        this.number=number;
+        this.agency=agency;
+        this.name=name;
+        this.balance=balance;
+    }
+
+    public void printAccountInfo() {
+        System.out.println("Número da conta: " + number);
+        System.out.println("Agência: " + agency);
+        System.out.println("Nome do titular: " + name);
+        System.out.println("Saldo: " + balance);
+    }
+}
